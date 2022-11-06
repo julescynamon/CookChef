174a25c85cb6aa9bc6a371a32c492d5624f0b238
cynamon jules <julescweb@mail.com>
2022-11-06 16:53:30 +0100

    refactoring finis

---
+++ src/hooks/useFetchRecipes.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { getRecipes } from '../apis';
+
+export function useFetchRecipes(page) {
+    const [recipes, setRecipes] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState([]);
+
+    useEffect(() => {
+        let cancel = false;
+        async function fetchData() {
+            try {
+                setIsLoading(true);
+                const queryParam = new URLSearchParams();
+                if (page) {
+                    queryParam.append('limit', 18);
+                    queryParam.append('skip', (page - 1) * 18);
+                    queryParam.append('sort', 'createdAt:-1');
+                }
+                const fetchedRecipes = await getRecipes(queryParam);
+                if (!cancel) {
+                    setRecipes((x) => [...x, ...fetchedRecipes]);
+                }
+            } catch (e) {
+                setError('Erreur');
+            } finally {
+                if (!cancel) {
+                    setIsLoading(false);
+                }
+            }
+        }
+        fetchData();
+        return () => (cancel = true);
+    }, [page]);
+
+    return [[recipes, setRecipes], isLoading, error];
+}
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,62 +1,67 @@
-import styles from "./Homepage.module.scss";
-import Recipe from "./components/Recipe/Recipe";
-import { useState } from "react";
-import Loading from "../../components/Loading/Loading";
-import { useContext } from "react";
-import { ApiContext } from "../../context/ApiContext";
-import Search from "./components/Search/Search";
-import { useFetchData } from "../../hooks";
+import styles from './Homepage.module.scss';
+import Recipe from './components/Recipe/Recipe';
+import { useState } from 'react';
+import Loading from '../../components/Loading/Loading';
+import Search from './components/Search/Search';
+import { useFetchRecipes } from '../../hooks';
+import { updateRecipe as updateR, deleteRecipe as deleteR } from '../../apis';
 
 function Homepage() {
-  const [filter, setFilter] = useState("");
-  const [page, setPage] = useState(1);
-  const BASE_URL_API = useContext(ApiContext);
-  const [[recipes, setRecipes], isLoading] = useFetchData(BASE_URL_API, page);
+    const [filter, setFilter] = useState('');
+    const [page, setPage] = useState(1);
+    const [[recipes, setRecipes], isLoading] = useFetchRecipes(page);
 
-  function updateRecipe(updatedRecipe) {
-    setRecipes(
-      recipes.map((r) => (r._id === updatedRecipe._id ? updatedRecipe : r))
-    );
-  }
+    async function updateRecipe(updatedRecipe) {
+        const savedRecipe = await updateR(updatedRecipe);
+        setRecipes(
+            recipes.map((r) => (r._id === savedRecipe._id ? savedRecipe : r))
+        );
+    }
 
-  function deleteRecipe(_id) {
-    setRecipes(recipes.filter((r) => r._id !== _id));
-  }
+    async function deleteRecipe(_id) {
+        await deleteR(_id);
+        setRecipes(recipes.filter((r) => r._id !== _id));
+    }
 
-  return (
-    <div className="flex-fill container d-flex flex-column p-20">
-      <h1 className="my-30">
-        Découvrez nos nouvelles recettes{" "}
-        <small className={styles.small}>- {recipes.length}</small>
-      </h1>
-      <div
-        className={`card flex-fill d-flex flex-column p-20 mb-20 ${styles.contentCard}`}
-      >
-        <Search setFilter={setFilter} />
-        {isLoading && !recipes.length ? (
-          <Loading />
-        ) : (
-          <div className={styles.grid}>
-            {recipes
-              .filter((r) => r.title.toLowerCase().startsWith(filter))
-              .map((r) => (
-                <Recipe
-                  key={r._id}
-                  recipe={r}
-                  deleteRecipe={deleteRecipe}
-                  toggleLikedRecipe={updateRecipe}
-                />
-              ))}
-          </div>
-        )}
-        <div className="d-flex flex-row justify-content-center align-items-center p-20">
-          <button onClick={() => setPage(page + 1)} className="btn btn-primary">
-            Charger plus de recettes
-          </button>
+    return (
+        <div className="flex-fill container d-flex flex-column p-20">
+            <h1 className="my-30">
+                Découvrez nos nouvelles recettes{' '}
+                <small className={styles.small}>- {recipes.length}</small>
+            </h1>
+            <div
+                className={`card flex-fill d-flex flex-column p-20 mb-20 ${styles.contentCard}`}
+            >
+                <Search setFilter={setFilter} />
+                {isLoading && !recipes.length ? (
+                    <Loading />
+                ) : (
+                    <div className={styles.grid}>
+                        {recipes
+                            .filter((r) =>
+                                r.title.toLowerCase().startsWith(filter)
+                            )
+                            .map((r) => (
+                                <Recipe
+                                    key={r._id}
+                                    recipe={r}
+                                    deleteRecipe={deleteRecipe}
+                                    updateRecipe={updateRecipe}
+                                />
+                            ))}
+                    </div>
+                )}
+                <div className="d-flex flex-row justify-content-center align-items-center p-20">
+                    <button
+                        onClick={() => setPage(page + 1)}
+                        className="btn btn-primary"
+                    >
+                        Charger plus de recettes
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Homepage;
